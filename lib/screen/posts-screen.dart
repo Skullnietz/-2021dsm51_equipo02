d7f16beb1fd2cc2aa4475ba76d07c6f52d51ec41
DSM51 Guizar Peña Carlos <al221910878@gmail.com>
2021-01-23 16:55:14 -0600

    14 Making a request to posts

--- lib/screen/posts-screen.dart
+++ lib/screen/posts-screen.dart
@@ -1,11 +1,30 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:dio/dio.dart' as Dio;
 import 'package:flutter/material.dart';
+import 'package:flutter_authentication_with_laravel_sanctum/models/post.dart';
+
+import '../dio.dart';
 
 class PostsScreen extends StatefulWidget {
   @override
-  _PostsScreenState createState() => _PostsScreenState();
+  State<StatefulWidget> createState() {
+    return PostsState();
+
+  }
 }
 
-class _PostsScreenState extends State<PostsScreen> {
+
+class PostsState extends State<PostsScreen> {
+   Future<List<Post>> getPosts() async {
+     Dio.Response response = await dio().get('user/posts');
+
+     List posts = json.decode(response.toString());
+
+     return posts.map((post)=>Post.fromJson(post)).toList();
+
+   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,7 +32,30 @@ class _PostsScreenState extends State<PostsScreen> {
         title:Text('Posts'),
          ),
          body: Center(
-           child:Text('Posts'),
+           child: FutureBuilder<List<Post>>(
+
+             future: getPosts(),
+             builder: (context, snapshot){
+               if(snapshot.hasData){
+                 return ListView.builder(
+                   itemCount: snapshot.data.length,
+                   itemBuilder: (context, index){
+                     var item = snapshot.data[index];
+
+                     return ListTile(
+                       title:Text(item.title),
+                     );
+                   }
+                   );
+
+               }else if (snapshot.hasError){
+                 log(snapshot.error.toString());
+                 return Text('Failed to load posts');
+               }
+
+               return CircularProgressIndicator();
+             }
+           ),
            ),
     );
   }
