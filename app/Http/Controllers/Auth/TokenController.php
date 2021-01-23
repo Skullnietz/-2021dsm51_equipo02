c5e7efedc9bb73c18f7ff71eb479d657763bb7f1
DSM51 Guizar Peña Carlos <al221910878@gmail.com>
2021-01-22 23:14:07 -0600

    07 Requesting a token

---
+++ app/Http/Controllers/Auth/TokenController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Auth\AuthenticationException;
+
+class TokenController extends Controller
+{
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'email'=> 'required|email',
+            'password'=> 'required',
+
+        ]);
+        if (!auth()->attempt($request->only('email','password'))) {
+            throw new AuthenticationException();
+        }
+
+        return[
+            'token' => auth()->user()->createToken('test')->plainTextToken
+
+        ];
+
+    }
+}
